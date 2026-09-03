a7ed5f498b0ed526bc8701c91884f9bc987f5900
Tibor Kiss <32801432+the1bit@users.noreply.github.com>
2026-09-04 01:33:01 +0200

    Add initial implementation of Chroma vector database examples with and without metadata (#1)

---
+++ vectordb/chroma/01-alap/chroma.py
@@ -0,0 +1,25 @@
+import chromadb
+chroma_client = chromadb.Client()
+
+
+collection = chroma_client.create_collection(name="alap_collection")
+
+
+collection.add(
+    ids=["id1", "id2", "id3", "id4"],
+    documents=[
+        "Ez egy dokumentum az iPhone-ról",
+        "Ez egy dokumentum az iPad-ről",
+        "Ez egy dokumentum a MacBook-ról",
+        "Ez egy dokumentum a Galaxy Z Fold-ról",
+    ]
+)
+
+
+results = collection.query(
+    query_texts=["Ez egy lekérdező dokumentum az Apple termékekről"], # Chroma will embed this for you
+    n_results=4 # how many results to return
+)
+# Eredmények kiíratása a konzolra
+print("Eredmények:")
+print(results)
